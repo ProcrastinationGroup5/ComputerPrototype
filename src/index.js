ccfb317d8264b74c934b22d9df0dfc30f848a3cc
Vishal Dhileepan <vishaldhileepan@Vishals-MacBook-Pro-2.local>
2019-11-13 15:44:36 -0600

    p6 second attempt

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { routerReducer } from 'react-router-redux';
+import Home from './Home/Home'
+import {
+  editTasks,
+  displayANTBool,
+  displayTaskInfoBool,
+  setTaskSelected
+} from './reducers';
+
+const rootReducer = combineReducers({
+  editTasks,
+  displayANTBool,
+  displayTaskInfoBool,
+  setTaskSelected
+})
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Home/>
+  </Provider>,
+  document.getElementById("root")
+);
